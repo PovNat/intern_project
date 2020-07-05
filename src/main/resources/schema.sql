101da21125bb28a41ae29150f96e07e9ce95eb2d
natalapovarova <n.povarova3101@yandex.ru>
2020-07-05 22:52:43 +0300

    Initial commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,71 @@
+CREATE TABLE IF NOT EXISTS organization(
+	id           INTEGER        NOT NULL  PRIMARY KEY  --COMMENT Уникальный ID организации
+    , name       VARCHAR(50)    NOT NULL               --COMMENT Название организации
+	, fullName   VARCHAR(50)    NOT NULL               --COMMENT Полное название
+	, address    VARCHAR(50)    NOT NULL               --COMMENT Адрес
+	, phone      VARCHAR(30)    NOT NULL               --COMMENT Телефон
+    , inn        VARCHAR(30)    NOT NULL               --COMMENT ИНН
+    , kpp        VARCHAR(30)    NOT NULL               --COMMENT КПП
+	, isActive   BOOLEAN                               --COMMENT Актуальность
+);
+
+CREATE TABLE IF NOT EXISTS officeIT(
+	orgId         INTEGER        NOT NULL              --COMMENT Уникальный ID организации
+	, id          INTEGER        NOT NULL  PRIMARY KEY --COMMENT Уникальный ID офиса
+    , name        VARCHAR(50)    NOT NULL              --COMMENT Название офиса',
+	, address     VARCHAR(50)    NOT NULL              --COMMENT Адрес
+    , phone       VARCHAR(30)                          --COMMENT Телефон
+	, isActive    BOOLEAN                              --COMMENT Актуальность
+);
+
+CREATE TABLE IF NOT EXISTS officeRusOil(
+	orgId         INTEGER        NOT NULL              --COMMENT Уникальный ID организации
+	, id          INTEGER        NOT NULL  PRIMARY KEY --COMMENT Уникальный ID офиса
+    , name        VARCHAR(50)    NOT NULL              --COMMENT Название офиса
+	, address     VARCHAR(50)    NOT NULL              --COMMENT Адрес
+    , phone       VARCHAR(30)                          --COMMENT Телефон
+	, isActive    BOOLEAN                              --COMMENT Актуальность
+);
+
+CREATE TABLE IF NOT EXISTS office4wheels(
+	orgId         INTEGER        NOT NULL              --COMMENT Уникальный ID организации
+	, id          INTEGER        NOT NULL  PRIMARY KEY --COMMENT Уникальный ID офиса
+    , name        VARCHAR(50)    NOT NULL              --COMMENT Название офиса
+	, address     VARCHAR(50)    NOT NULL              --COMMENT Адрес
+    , phone       VARCHAR(30)                          --COMMENT Телефон
+	, isActive    BOOLEAN                              --COMMENT Актуальность
+);
+
+CREATE TABLE IF NOT EXISTS position(
+    positionId      INTEGER     NOT NULL PRIMARY KEY  --COMMENT Уникальный ID должности
+	, positionName  VARCHAR(30) NOT NULL              --COMMENT Название должности
+);
+
+CREATE TABLE IF NOT EXISTS document(
+	documentID       INTEGER       NOT NULL PRIMARY KEY   --COMMENT ID документа
+	, documentName   VARCHAR(30)   NOT NULL               --COMMENT Название документа
+	, documentCode   INTEGER       NOT NULL               --COMMENT Код документа
+);
+
+CREATE TABLE IF NOT EXISTS citizenship(
+	citizenshipID       INTEGER      NOT NULL   PRIMARY KEY  --COMMENT ID страны
+	, citizenshipName   VARCHAR(30)  NOT NULL                --COMMENT Гражданство
+	, citizenshipCode   INTEGER      NOT NULL                --COMMENT Код страны
+
+);
+
+CREATE TABLE IF NOT EXISTS users(
+	id                  INTEGER        NOT NULL  PRIMARY KEY  --COMMENT Уникальный ID сотрудника
+	, orgId             INTEGER                               --СOMMENT ID  организации
+	, officeId          INTEGER        NOT NULL               --СOMMENT ID офиса
+	, firstName         VARCHAR(50)    NOT NULL               --COMMENT Имя
+    , secondName        VARCHAR(50)                           --COMMENT Фамилия
+    , middleName        VARCHAR(50)                           --COMMENT Отчество
+    , position          INTEGER        NOT NULL               --COMMENT Должность
+    , phone             VARCHAR(30)                           --COMMENT Телефон
+    , docName           INTEGER                               --COMMENT Название документа
+    , docNumber         VARCHAR(20)                           --COMMENT Номер документа
+    , docDate           VARCHAR(20)                           --COMMENT Дата выдачи
+    , citizenshipCode   INTEGER                               --COMMENT Код страны
+    , isIdentified      BOOLEAN                               --COMMENT Атуальность
+);
---
+++ src/test/java/ru/bellintegrator/practice/intern_project/intern_project/InternProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bellintegrator.practice.intern_project.intern_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
