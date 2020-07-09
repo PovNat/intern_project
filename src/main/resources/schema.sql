3fd91fc265d4c78e8efde9338fabb24c03ec267e
natalapovarova <n.povarova3101@yandex.ru>
2020-07-09 15:43:23 +0300

    Собранные таблицы;индексы;внешн.ключи

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,50 +9,22 @@ CREATE TABLE IF NOT EXISTS organization(
 	, isActive   BOOLEAN                               --COMMENT Актуальность
 );
 
-CREATE TABLE IF NOT EXISTS officeIT(
+CREATE UNIQUE INDEX orname_idx ON organization (name);
+CREATE UNIQUE INDEX oractive_idx ON organization (isActive);
+
+create table if not exists offices(
 	orgId         INTEGER        NOT NULL              --COMMENT Уникальный ID организации
 	, id          INTEGER        NOT NULL  PRIMARY KEY --COMMENT Уникальный ID офиса
     , name        VARCHAR(50)    NOT NULL              --COMMENT Название офиса
 	, address     VARCHAR(50)    NOT NULL              --COMMENT Адрес
     , phone       VARCHAR(30)                          --COMMENT Телефон
 	, isActive    BOOLEAN                              --COMMENT Актуальность
+	, FOREIGN KEY (orgId) REFERENCES organization (ID)
 );
 
-CREATE TABLE IF NOT EXISTS officeRusOil(
-	orgId         INTEGER        NOT NULL              --COMMENT Уникальный ID организации
-	, id          INTEGER        NOT NULL  PRIMARY KEY --COMMENT Уникальный ID офиса
-    , name        VARCHAR(50)    NOT NULL              --COMMENT Название офиса
-	, address     VARCHAR(50)    NOT NULL              --COMMENT Адрес
-    , phone       VARCHAR(30)                          --COMMENT Телефон
-	, isActive    BOOLEAN                              --COMMENT Актуальность
-);
+CREATE INDEX oforgid_idx ON offices (orgId);
+CREATE INDEX ofactive_idx ON offices (isActive);
 
-CREATE TABLE IF NOT EXISTS office4wheels(
-	orgId         INTEGER        NOT NULL              --COMMENT Уникальный ID организации
-	, id          INTEGER        NOT NULL  PRIMARY KEY --COMMENT Уникальный ID офиса
-    , name        VARCHAR(50)    NOT NULL              --COMMENT Название офиса
-	, address     VARCHAR(50)    NOT NULL              --COMMENT Адрес
-    , phone       VARCHAR(30)                          --COMMENT Телефон
-	, isActive    BOOLEAN                              --COMMENT Актуальность
-);
-
-CREATE TABLE IF NOT EXISTS position(
-    positionId      INTEGER     NOT NULL PRIMARY KEY  --COMMENT Уникальный ID должности
-	, positionName  VARCHAR(30) NOT NULL              --COMMENT Название должности
-);
-
-CREATE TABLE IF NOT EXISTS document(
-	documentID       INTEGER       NOT NULL PRIMARY KEY   --COMMENT ID документа
-	, documentName   VARCHAR(30)   NOT NULL               --COMMENT Название документа
-	, documentCode   INTEGER       NOT NULL               --COMMENT Код документа
-);
-
-CREATE TABLE IF NOT EXISTS citizenship(
-	citizenshipID       INTEGER      NOT NULL   PRIMARY KEY  --COMMENT ID страны
-	, citizenshipName   VARCHAR(30)  NOT NULL                --COMMENT Гражданство
-	, citizenshipCode   INTEGER      NOT NULL                --COMMENT Код страны
-
-);
 
 CREATE TABLE IF NOT EXISTS users(
 	id                  INTEGER        NOT NULL  PRIMARY KEY  --COMMENT Уникальный ID сотрудника
@@ -61,11 +33,43 @@ CREATE TABLE IF NOT EXISTS users(
 	, firstName         VARCHAR(50)    NOT NULL               --COMMENT Имя
     , secondName        VARCHAR(50)                           --COMMENT Фамилия
     , middleName        VARCHAR(50)                           --COMMENT Отчество
-    , position          INTEGER        NOT NULL               --COMMENT Должность
+    , position          VARCHAR(30)    NOT NULL               --COMMENT Должность
     , phone             VARCHAR(30)                           --COMMENT Телефон
-    , docName           INTEGER                               --COMMENT Название документа
-    , docNumber         VARCHAR(20)                           --COMMENT Номер документа
-    , docDate           VARCHAR(20)                           --COMMENT Дата выдачи
-    , citizenshipCode   INTEGER                               --COMMENT Код страны
     , isIdentified      BOOLEAN                               --COMMENT Атуальность
 );
+
+CREATE INDEX usorgid_idx ON users (orgId);
+CREATE INDEX usofid_idx ON users (officeId);
+CREATE INDEX uspos_idx ON users (position);
+CREATE INDEX usactive_idx ON users (isIdentified);
+
+
+CREATE TABLE IF NOT EXISTS docname(
+	documentID       INTEGER       NOT NULL  PRIMARY KEY  --COMMENT ID документа
+	, documentName   VARCHAR(30)   NOT NULL               --COMMENT Название документа
+	, documentCode   INTEGER       NOT NULL               --COMMENT Код документа
+	, UNIQUE(documentID,documentName, documentCode)
+);
+
+
+CREATE TABLE IF NOT EXISTS docdata(
+    usID      INTEGER     NOT NULL PRIMARY KEY   -- COMMENT ID сотрудника
+    , docID       INTEGER       NOT NULL        --COMMENT ID документа
+    , docNumber         VARCHAR(20)               --COMMENT Номер документа
+    , docDate           VARCHAR(20)               --COMMENT Дата выдачи
+    , citizenshipID       INTEGER   NOT NULL    --COMMENT ID страны
+	, UNIQUE(usID)
+    , FOREIGN KEY (usID) REFERENCES users (id)
+	, FOREIGN KEY (docID) REFERENCES docname (documentID)
+	, FOREIGN KEY (citizenshipID) REFERENCES citizenship (citizenshipID)
+);
+
+
+CREATE TABLE IF NOT EXISTS citizenship(
+	citizenshipID       INTEGER      NOT NULL   PRIMARY KEY  --COMMENT ID страны
+	, citizenshipName   VARCHAR(30)  NOT NULL                --COMMENT Гражданство
+	, citizenshipCode   INTEGER      NOT NULL                --COMMENT Код страны
+);
+select * from citizenship
+drop table citizenship
+
