7262c868e37a2b0cc2d6e35a0c77cacf54a439de
natalapovarova <n.povarova3101@yandex.ru>
2020-07-13 20:36:42 +0300

    import

--- src/main/java/ru/bellintegrator/practice/intern_project/intern_project/model/Docdata.java
+++ src/main/java/ru/bellintegrator/practice/intern_project/intern_project/model/Docdata.java
@@ -1,6 +1,15 @@
 package ru.bellintegrator.practice.intern_project.intern_project.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import javax.persistence.OneToOne;
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 
 @Entity(name = "Docdata")
 public class Docdata {
